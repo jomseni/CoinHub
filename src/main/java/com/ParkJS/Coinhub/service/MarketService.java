b30b1a05235c66649191b2adb09d075ce43667e2
jomseni <108383921+jomseni@users.noreply.github.com>
2022-11-15 02:30:50 +0900

    MarketService라는 인터 페이스를 사용하는 클래스들은 그에 대한 메서드를 꼭 만들어 주어야 한다!

--- src/main/java/com/ParkJS/Coinhub/service/MarketService.java
+++ src/main/java/com/ParkJS/Coinhub/service/MarketService.java
@@ -1,6 +1,6 @@
 package com.ParkJS.Coinhub.service;
 
 public interface MarketService {
-
+//MarketService라는 인터 페이스를 사용하는 클래스들은 그에 대한 메서드를 꼭 만들어 주어야 한다!
     double getCoinCurrentPrice(String coin);
 }
