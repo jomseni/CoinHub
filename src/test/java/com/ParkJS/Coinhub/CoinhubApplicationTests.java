fe225a0a6515a5ad96282f714767fcb36c26e655
jomseni <108383921+jomseni@users.noreply.github.com>
2022-11-14 20:33:20 +0900

    Initial commit

---
+++ src/test/java/com/ParkJS/Coinhub/CoinhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ParkJS.Coinhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
